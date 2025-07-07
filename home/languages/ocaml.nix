22599fe9c91826068b9bf847223d59e46b602f5c
MinimumAction <liguori.km@gmail.com>
2025-07-07 14:39:49 -0400

    added ocamlfind

--- home/languages/ocaml.nix
+++ home/languages/ocaml.nix
@@ -1,20 +1,15 @@
 { config, lib, pkgs, osConfig, ... }:
 
 {
-  home.packages = with pkgs; [
-    # Core OCaml tooling
-    ocaml       # OCaml
-    dune_3      # Dune build system
-    ocamlformat # Auto formatting
-
-    # OCaml tools
-    ocamlPackages.findlib      # Dynamically find ocaml packages for use in utop
-    ocamlPackages.utop         # OCaml toplevel
-    ocamlPackages.merlin       # Editor integration
-    ocamlPackages.ocaml-lsp    # Language Server Protocol (for VS Code, Emacs, etc.)
-    
-    # OCaml Packages/Libraries
-    ocamlPackages.base         # Jane Street's standard lib
-    ocamlPackages.stdio
+  home.packages = with pkgs.ocamlPackages; [
+    ocaml
+    dune_3
+    merlin
+    ocamlfind
+    ocaml-lsp
+    utop
+    base
+    stdio
+    ppx_jane
   ];
 }
