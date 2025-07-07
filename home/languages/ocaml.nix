89a1e4ea605ffd31ac163ce19e4023cca50afdbc
MinimumAction <liguori.km@gmail.com>
2025-07-07 14:14:49 -0400

    back to user level OCaml

--- home/languages/ocaml.nix
+++ home/languages/ocaml.nix
@@ -3,15 +3,17 @@
 {
   home.packages = with pkgs; [
     # Core OCaml tooling
-    ocaml
-    dune_3
-    # opam  # will try not to use, want to manage packages with nix. 
+    ocaml  # OCaml
+    dune_3 # Dune build system
 
-    # OCaml libraries/tools
+    # OCaml tools
     ocamlPackages.findlib      # Dynamically find ocaml packages for use in utop
-    ocamlPackages.base         # Jane Street's standard lib
-    ocamlPackages.utop         # REPL
+    ocamlPackages.utop         # OCaml toplevel
     ocamlPackages.merlin       # Editor integration
     ocamlPackages.ocaml-lsp    # Language Server Protocol (for VS Code, Emacs, etc.)
+    
+    # OCaml Packages/Libraries
+    ocamlPackages.base         # Jane Street's standard lib
+    ocamlPackages.stdio
   ];
 }
