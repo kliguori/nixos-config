19547f23a1e546686060b2f69136b1e25d1fc18f
MinimumAction <liguori.km@gmail.com>
2025-07-09 23:00:20 -0400

    vscode config fixed?

--- home/vscode/vscode.nix
+++ home/vscode/vscode.nix
@@ -3,17 +3,17 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode; 
+    package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       haskell.haskell
     ];
-  };
 
-  userSettings = {
-    "workbench.colorTheme" = "Dracula";
-    "editor.formatOnSave" = true;
-    "haskell.formattingProvider" = "ormolu";
-    "files.insertFinalNewline" = true;
+    userSettings = {
+      "workbench.colorTheme" = "Dracula";
+      "editor.formatOnSave" = true;
+      "haskell.formattingProvider" = "ormolu";
+      "files.insertFinalNewline" = true;
+    };
   };
 }
