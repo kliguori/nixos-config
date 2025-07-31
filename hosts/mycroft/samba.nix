50aede70e2fde74f11409eaeb83e3c99df7b8b33
kevin <liguori.km@gmail.com>
2025-07-31 00:38:45 -0400

    fixed samba...............?

--- hosts/mycroft/samba.nix
+++ hosts/mycroft/samba.nix
@@ -1,9 +1,26 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.groups = {
-    sambakevin = { };
-    sambajane = { };
+  users.groups.sambakevin = { };
+  users.users.kevin = {
+    isNormalUser = true;
+    description = "Samba user Kevin";
+    group = "sambakevin";         # primary group
+    extraGroups = [ ];            # no extra groups
+    createHome = false;           # no home directory
+    shell = pkgs.nologin;         # prevent shell login
+    password = "!" ;              # disable system login password
+  };
+
+  users.groups.sambajane = { };
+  users.users.jane = {
+    isNormalUser = true;
+    description = "Samba user Jane";
+    group = "sambajane";          # primary group
+    extraGroups = [ ];            # no extra groups
+    createHome = false;           # no home directory
+    shell = pkgs.nologin;         # prevent shell login
+    password = "!" ;              # disable system login password
   };
 
   systemd.tmpfiles.rules = [
@@ -20,14 +37,12 @@
         "server string" = "smbnix";
         "netbios name" = "smbnix";
         "security" = "user";
-        #"use sendfile" = "yes";
-        #"max protocol" = "smb2";
-        # note: localhost is the ipv6 localhost ::1
-        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts allow" = "100.64.54. 10.54.1. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
       };
+      
       "kevin" = {
         "path" = "/srv/users/kevin";
         "browseable" = "yes";
@@ -38,16 +53,17 @@
         "force user" = "kevin";
         "force group" = "sambakevin";
       };
-    #   "jane" = {
-    #     "path" = "/srv/users/jane";
-    #     "browseable" = "yes";
-    #     "read only" = "no";
-    #     "guest ok" = "no";
-    #     "create mask" = "0644";
-    #     "directory mask" = "0755";
-    #     "force user" = "jane";
-    #     "force group" = "sambajane";
-    #   };
+      
+      "jane" = {
+        "path" = "/srv/users/jane";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "jane";
+        "force group" = "sambajane";
+      };
     };
   };
 }
