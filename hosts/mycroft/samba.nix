c086bd3e6c7a9f58addd8eeae17c01eeb303fc08
kevin <liguori.km@gmail.com>
2025-07-30 23:50:36 -0400

    added samba

---
+++ hosts/mycroft/samba.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, ... }:
+
+{
+  users.groups = {
+    sambakevin = { };
+    sambajane = { };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /srv/users/kevin 0770 root sambakevin - -"
+    "d /srv/users/jane  0770 root sambajane  - -"
+  ];
+
+  services.samba = {
+    enable = true;
+    openFirewall = false;  
+    
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = NixOS NAS
+      netbios name = nixnas
+      security = user
+      map to guest = never
+      vfs objects = acl_xattr
+      smb encrypt = required
+      interfaces = tailscale0
+      bind interfaces only = yes
+    '';  
+    
+    shares = {
+      kevin = {
+        path = "/srv/users/kevin";
+        browseable = true;
+        writable = true;
+        "read only" = false;
+        "valid users" = [ "kevin" ];
+        "force group" = "sambakevin";
+        "create mask" = "0660";
+        "directory mask" = "0770";
+      };  
+    
+      #jane = {
+      #  path = "/srv/users/jane";
+      #  browseable = true;
+      #  writable = true;
+      #  "read only" = false;
+      #  "valid users" = [ "jane" ];
+      #  "force group" = "sambajane";
+      #  "create mask" = "0660";
+      #  "directory mask" = "0770";
+      #};
+    };
+  };
+}
