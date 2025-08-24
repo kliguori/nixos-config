1ec83dad98466d3632246b33f7e7d9cba7426ab2
kliguori <liguori.km@gmail.com>
2025-08-24 17:52:19 -0400

    persist libvirt

--- hosts/sherlock/file-system.nix
+++ hosts/sherlock/file-system.nix
@@ -1,68 +1,98 @@
 { config, lib, pkgs, ... }: 
 
 {
-  # Swap devices
+  # No swap devices
   swapDevices = [ ];
 
-  # Root filesystem
-  fileSystems."/" = { 
-    device = "rpool/root";
-    fsType = "zfs";
-  };
-
-  # Nix store filesystem
-  fileSystems."/nix" = { 
-    device = "rpool/nix";
-    fsType = "zfs";
-  };
-
-  # Persistent storage filesystem
-  fileSystems."/persist" = { 
-    device = "rpool/persist";
-    fsType = "zfs";
-  };
-
-  # Boot filesystem
+  # ---------------------------------------------
+  # Boot partition
+  # /boot
+  # ---------------------------------------------
   fileSystems."/boot" = { 
     device = "/dev/disk/by-label/NIXBOOT";
     fsType = "vfat";
     options = [ "fmask=0022" "dmask=0022" ];
   };
 
-  # Persist NetworkManager connections by bind-mounting to /persist
-  fileSystems."/etc/NetworkManager/system-connections" = {
-    device = "/persist/etc/NetworkManager/system-connections";
-    options = [ "bind" ];
+  # ---------------------------------------------
+  # Mountpoints for rpool datasets
+  # ---------------------------------------------
+  # root dataset
+  fileSystems."/" = { 
+    device = "rpool/root";
+    fsType = "zfs";
   };
 
-  # Persist Tailscale state by bind-mounting to /persist
-  fileSystems."/var/lib/tailscale" = {
-    device = "/persist/var/lib/tailscale";
-    options = [ "bind" ];
+  # nix dataset
+  fileSystems."/nix" = { 
+    device = "rpool/nix";
+    fsType = "zfs";
   };
 
-  # Persist SSH keys by bind-mounting to /persist
-  fileSystems."/home/kevin/.ssh" = {
-    device = "/persist/home/kevin/.ssh";
-    options = [ "bind" ];
+  # persist dataset
+  fileSystems."/persist" = { 
+    device = "rpool/persist";
+    fsType = "zfs";
   };
 
-  # Persist virtual machines
+  # ---------------------------------------------
+  # Mountpoints for dpool datasets
+  # ---------------------------------------------
+  # vmachines dataset
   fileSystems."/vmachines" = { 
     device = "dpool/crypt/vmachines";
     fsType = "zfs";
   };
 
-  # Persist Ubuntu dev machine
+  # ubuntu-dev dataset
   fileSystems."/vmachines/ubuntu-dev" = { 
     device = "dpool/crypt/vmachines/ubuntu-dev";
     fsType = "zfs";
   };
 
-  # Persist the ripper
+  # the-ripper dataset
   fileSystems."/vmachines/the-ripper" = { 
     device = "dpool/crypt/vmachines/the-ripper";
     fsType = "zfs";
+  };
+
+  # ---------------------------------------------
+  # Directories to bind mount to /etc
+  # ---------------------------------------------
+  # network connections
+  fileSystems."/etc/NetworkManager/system-connections" = {
+    device = "/persist/etc/NetworkManager/system-connections";
+    options = [ "bind" ];
+  };
+
+  # libvirt networks and config
+  fileSystems."/etc/libvirt" = {
+    device = "/persist/etc/libvirt";
+    options = [ "bind" ];
+  };
+
+  # ---------------------------------------------
+  # Directories to bind mount to /var/lib
+  # ---------------------------------------------
+  # tailscale state
+  fileSystems."/var/lib/tailscale" = {
+    device = "/persist/var/lib/tailscale";
+    options = [ "bind" ];
+  };
+
+  # libvirt state
+  fileSystems."/var/lib/libvirt" = {
+    device = "/persist/var/lib/libvirt";
+    options = [ "bind" ];
+  };
+
+  # ---------------------------------------------
+  # Directories to bind mount to /home
+  # ---------------------------------------------
+  # ssh keys
+  fileSystems."/home/kevin/.ssh" = {
+    device = "/persist/home/kevin/.ssh";
+    options = [ "bind" ];
   };
 
 }
