20a74c702db7658da6c0322af15a55a15a851d94
kliguori <liguori.km@gmail.com>
2025-08-24 22:35:20 -0400

    fixed mistake

--- hosts/sherlock/systemd.nix
+++ hosts/sherlock/systemd.nix
@@ -5,7 +5,7 @@
     # Set permissions in /persist for directories that are bind-mounted (also creates directories if they don't exist)
     tmpfiles.rules = [
       "d /persist/etc/NetworkManager/system-connections 0700 root root - -"   # NetworkManager connections
-      "d /vmachines 0775 root kvm - -"                                        # Virtual machine images
+      "z /vmachines 0775 root kvm - -"                                        # Virtual machine images
       "d /persist/var/lib/tailscale 0700 root root - -"                       # Tailscale state
       "d /persist/var/lib/libvirt 0755 root root - -"                         # Libvirt state
       "d /persist/home/kevin/.ssh 0700 kevin users - -"                       # SSH keys
