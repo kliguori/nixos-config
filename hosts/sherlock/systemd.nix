1c96089725a56cb975c7e827f285d7b68d89e699
kliguori <liguori.km@gmail.com>
2025-08-24 22:29:12 -0400

    updated some permissions to let qemu user write

--- hosts/sherlock/systemd.nix
+++ hosts/sherlock/systemd.nix
@@ -5,7 +5,7 @@
     # Set permissions in /persist for directories that are bind-mounted (also creates directories if they don't exist)
     tmpfiles.rules = [
       "d /persist/etc/NetworkManager/system-connections 0700 root root - -"   # NetworkManager connections
-      # "d /persist/etc/libvirt 0755 root root - -"                             # Libvirt network and config
+      "d /vmachines 0757 root kvm - -"                                        # Virtual machine images
       "d /persist/var/lib/tailscale 0700 root root - -"                       # Tailscale state
       "d /persist/var/lib/libvirt 0755 root root - -"                         # Libvirt state
       "d /persist/home/kevin/.ssh 0700 kevin users - -"                       # SSH keys
