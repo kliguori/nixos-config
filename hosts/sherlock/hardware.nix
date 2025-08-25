2fb2be86bfb807f6ee8e183aea4d8ef89282b9a0
kliguori <liguori.km@gmail.com>
2025-08-25 09:28:55 -0400

    changed some nvidia settings to fix the suspend issue...

--- hosts/sherlock/hardware.nix
+++ hosts/sherlock/hardware.nix
@@ -7,7 +7,7 @@
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
-      powerManagement.finegrained = true;
+      powerManagement.finegrained = false;
       open = true;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
