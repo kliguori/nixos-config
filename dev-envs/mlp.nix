197b80dc780750a001df9a4d6012a785d9cead94
kliguori <liguori.km@gmail.com>
2025-08-26 17:23:11 -0400

    added mpl dev env......

--- dev-envs/mlp.nix
+++ dev-envs/mlp.nix
@@ -1,31 +1,29 @@
 { pkgs, ... }:
 
-let
-  py = pkgs.python312;
-in {
-  pkgs.mkShell {
-    name = "mlp";
-    packages = [
-      (py.withPackages (ps: with ps; [
-        numpy
-        scipy
-        sympy
-        pandas
-        scikit-learn
-        torch-bin
-        torchvision-bin
-        jupyterlab
-        matplotlib
-        seaborn
-        ffmpeg
-        tqdm
-      ]))
-    ];
+pkgs.mkShell {
+  name = "mlp";
+  
+  packages = with pkgs; [
+    (python312.withPackages (ps: with ps; [
+      numpy
+      scipy
+      sympy
+      pandas
+      scikit-learn
+      torch-bin
+      torchvision-bin
+      jupyterlab
+      matplotlib
+      seaborn
+      ffmpeg
+      tqdm
+    ]))
+  ];
 
-    shellHook = ''
-      export NIX_DEV_SHELL_NAME=mlp
-      export SHELL=${pkgs.zsh}/bin/zsh
-      exec ${pkgs.zsh}/bin/zsh --login
-    '';
-  }
+  shellHook = ''
+    export NIX_DEV_SHELL_NAME=mlp
+    export SHELL=${pkgs.zsh}/bin/zsh
+    exec ${pkgs.zsh}/bin/zsh --login
+  '';
 }
+
