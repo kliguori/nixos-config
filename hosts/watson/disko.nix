569ff7771bde60539dbb10c7ba0deb25c3e90f7f
kliguori <liguori.km@gmail.com>
2025-08-28 11:02:41 -0400

    flake version of disko

--- hosts/watson/disko.nix
+++ hosts/watson/disko.nix
@@ -1,100 +1,98 @@
 { ... }:
 
 {
-  disko.devices = {
-    disk.main = {
-      device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HBJQ-000L7_S4ENNX1R291121";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            name = "ESP";
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [
-                "fmask=0022"
-                "dmask=0022"
-              ];
-              extraArgs = [
-                "-n"
-                "NIXBOOT"
-              ];
-            };
+  disk.main = {
+    device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HBJQ-000L7_S4ENNX1R291121";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          name = "ESP";
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [
+              "fmask=0022"
+              "dmask=0022"
+            ];
+            extraArgs = [
+              "-n"
+              "NIXBOOT"
+            ];
           };
+        };
 
-          luks = {
-            name = "cryptroot";
-            size = "100%";
+        luks = {
+          name = "cryptroot";
+          size = "100%";
+          content = {
+            type = "luks";
+            name = "cryptroot"; # /dev/mapper/cryptroot
+            settings.label = "cryptroot";
             content = {
-              type = "luks";
-              name = "cryptroot"; # /dev/mapper/cryptroot
-              settings.label = "cryptroot";
-              content = {
-                type = "zfs";
-                pool = "rpool";
-              };
+              type = "zfs";
+              pool = "rpool";
             };
           };
         };
       };
     };
+  };
 
-    zpool.rpool = {
-      type = "zpool";
+  zpool.rpool = {
+    type = "zpool";
 
-      options = {
-        ashift = "12";
-        autotrim = "on";
+    options = {
+      ashift = "12";
+      autotrim = "on";
+    };
+
+    rootFsOptions = {
+      mountpoint = "none";
+      compression = "zstd";
+      atime = "off";
+      xattr = "sa";
+      acltype = "posixacl";
+    };
+
+    datasets = {
+      root = {
+        type = "zfs_fs";
+        mountpoint = "/";
+        options = {
+          canmount = "noauto";
+        };
       };
 
-      rootFsOptions = {
-        mountpoint = "none";
-        compression = "zstd";
-        atime = "off";
-        xattr = "sa";
-        acltype = "posixacl";
+      nix = {
+        type = "zfs_fs";
+        mountpoint = "/nix";
+        options = {
+          atime = "off";
+        };
       };
 
-      datasets = {
-        root = {
-          type = "zfs_fs";
-          mountpoint = "/";
-          options = {
-            canmount = "noauto";
-          };
-        };
+      persist = {
+        type = "zfs_fs";
+        mountpoint = "/persist";
+      };
 
-        nix = {
-          type = "zfs_fs";
-          mountpoint = "/nix";
-          options = {
-            atime = "off";
-          };
-        };
+      kevin = {
+        type = "zfs_fs";
+        mountpoint = "/home/kevin";
+      };
 
-        persist = {
-          type = "zfs_fs";
-          mountpoint = "/persist";
-        };
-
-        kevin = {
-          type = "zfs_fs";
-          mountpoint = "/home/kevin";
-        };
-
-        crypt = {
-          type = "zfs_fs";
-          mountpoint = "/home/kevin/crypt";
-          options = {
-            encryption = "aes-256-gcm";
-            keyformat = "passphrase";
-            keylocation = "prompt";
-          };
+      crypt = {
+        type = "zfs_fs";
+        mountpoint = "/home/kevin/crypt";
+        options = {
+          encryption = "aes-256-gcm";
+          keyformat = "passphrase";
+          keylocation = "prompt";
         };
       };
     };
